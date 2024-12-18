9b4a4b9e50974a95250800c23f0b642d522f2848
Martin <martin_0590@hotmail.com>
2024-12-18 12:10:48 -0300

    added - styles & fonts

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth"
+import GitHubProvider from "next-auth/providers/github"
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!
+    })
+  ]
+})
+export { handler as GET, handler as POST}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
+import React from 'react'
+const Navbar =  () => {
+  const {data: session} = useSession()
+
+  return (
+    <div className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+      <nav className='flex justify-between items-center'>
+        <Link
+          href='/'
+          className='text-black'
+        >
+          <span>G</span>ame <span>R</span>evies
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+            <Link href={"/startup/create"}>
+              <span>Create</span>
+            </Link>
+
+            <button onClick={() => signOut({callbackUrl: "/"})}>
+              <span>Logout</span>
+            </button>
+
+            <Link href={`/user/${session?.user.name}`}>
+              <span>{session?.user?.name}</span>
+            </Link>
+            </>
+          ): (
+              <button onClick={() => signIn("github")}>
+                <span>Login</span>
+              </button>
+          )}
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
